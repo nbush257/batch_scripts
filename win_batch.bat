d605d595fe588d029f435fc61223a1693b440260
nbush257 <nbush257@gmail.com>
2018-11-27 14:24:55 -0600

    overwrite wasnt working. Trying a new overwrite attempt

--- win_batch.bat
+++ win_batch.bat
@@ -4,17 +4,10 @@
   /log="C:\Users\guru\Desktop\winscp_logs\winscplog.log" /ini=nul ^
   /command ^
     "option confirm off" ^
+    "option batch on" ^
     "open ftp://nicholasbush2012%%40u.northwestern.edu:Bnji9olqaz%%21@ftp.box.com/ -rawsettings ProxyPort=0" ^
     "put ""F:\\upload\*.*"" ""/__AWAKE_backup/avi/*.*""" ^
     "exit"
-
-set WINSCP_RESULT=%ERRORLEVEL%
-if %WINSCP_RESULT% equ 0 (
-  echo Success
-  del "F:\upload\*.*"
-) else (
-  echo Error
-)
 
 pause
 exit /b %WINSCP_RESULT%
